6e4da483a675c37cd923c27d7952667535d4bec9
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-09-21 08:12:10 -0500

    🔥 Add offer store infos

--- src/modules/offer.py
+++ src/modules/offer.py
@@ -8,10 +8,24 @@ class Offer:
         """
         return self.link == other.link
 
-    def infos(self):
+    def store_infos(
+        self, 
+        job_title:str, 
+        company:str, 
+        location:str, 
+        job_metadata:str, 
+        job_description:str,
+        job_profile:str,
+        skills:str):
         """
         """
-        pass
+        self.job_title = job_title # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[1]/div[1]/h1".text
+        self.company = company # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div".text
+        self.location = location # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]".text
+        self.job_metadata = job_metadata # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/span".text
+        self.job_description = job_description # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[4]/div[3]/div/div[1]".text
+        self.job_profile = job_profile # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[4]/div[3]/div/div[2]/div" multiple li and text
+        self.skills = skills # "/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/div[1]/div[4]/div[3]/div/div[5]/div[1]/div" multiple text in div
 
     def apply(self):
         """
