a469ca6bfda76b0bdd8b1b299861942ee40f3dd5
PeregHer <pereg.hergoualch@isen-ouest.yncrea.fr>
2021-09-21 16:50:31 +0200

    🎈 added txt

--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -21,21 +21,39 @@ class Indeed:
     def save_offers(self):
         """
         """
-        offers = self.driver.find_elements_by_class_name('resultWithShelf')
+        offers = self.driver.find_elements_by_xpath('//*[@id="mosaic-provider-jobcards"]/a')
         for e in offers:
-            offer = Offer(e.get_attribute('href'))
-            print(offer.link)
+            job_id = e.get_attribute('id').split('_')[-1]
+            offer = Offer(f'https://fr.indeed.com/voir-emploi?jk={job_id}')
             if offer not in self.offers:
                 self.offers.append(offer)
     
+    def export_to_txt(self):
+        """
+        """
+        with open('indeed.txt', 'w') as file:
+            for offer in self.offers:
+                file.write(offer.link + '\n')
+
+
     def scrap(self):
         """
         """
         i = 0
         while True:
             self.url = f'https://fr.indeed.com/emplois?q={self.job}&l={self.location}&start={i}'
+
             self.access_page(self.url)
+            try: 
+                self.driver.find_element_by_xpath('//*[@id="mosaic-provider-jobcards"]/a')
+                print('I', i)
+                i += 10
+            except: continue
             self.save_offers()
-            i += 10
-            print(len(self.offers))
+            
+            print('LEN', len(self.offers))
+
+            if len(self.offers) > 150: 
+                self.export_to_txt()
+                break
             
