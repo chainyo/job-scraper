ceacf56f4ab0edb10083bc5449415c5ff8b54c11
PeregHer <pereg.hergoualch@isen-ouest.yncrea.fr>
2021-09-21 15:22:14 +0200

    💥 scraping offers

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 
-from src.modules.scraper import Scraper
+from modules.scraper import Indeed
 
 try:
     grid_url = "http://127.0.0.1:4444/wd/hub"
     driver = webdriver.Remote(command_executor=grid_url)
 
-    scraper = Scraper(url="http://fr.indeed.com", driver=driver, job="intelligence artificielle", location="Lyon")
+    scraper = Indeed(driver=driver, job="data", location="Lyon")
     scraper.scrap()
 
     # save screenshot
--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -1,54 +1,50 @@
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
 
-from src.modules.offer import Offer
+from modules.offer import Offer
 
-class Scraper:
 
-    def __init__(self, url:str, job:str, location:str, driver:webdriver):
+class Indeed:
+
+    def __init__(self, job:str, location:str, driver:webdriver):
         """
         """
-        self.url = url
         self.location = location
         self.job = job
         self.driver = driver
         self.offers = []
 
-    def get_page(self):
+    def get_page(self, url:str):
         """
         """
-        self.driver.get(self.url)
-
-    def launch_search(self):
-        """
-        """
-        self.driver.find_element_by_id("text-input-what").send_keys("intelligence artificielle")
-        where_input = self.driver.find_element_by_id("text-input-where")
-        where_input.send_keys(Keys.CONTROL + "a")
-        where_input.send_keys(Keys.DELETE)
-        where_input.send_keys("Lyon")
-
-        self.driver.find_element_by_id("text-input-what").submit()
+        self.driver.get(url)
 
     def save_offers(self):
         """
         """
-        offers = self.driver.find_elements_by_xpath('//*[@id="mosaic-provider-jobcards"]/a')
+        offers = self.driver.find_elements_by_class_name('resultWithShelf')
+        print(offers)
         for e in offers:
             offer = Offer(e.get_attribute('href'))
+            print(offer.link)
             if offer not in self.offers:
                 self.offers.append(offer)
+                return True
             else: return False
     
     def scrap(self):
         """
         """
-        self.get_page()
-        self.launch_search()
         i = 0
         while True:
+            self.driver.save_screenshot("indeed1.png")
+            self.url = f'https://fr.indeed.com/emplois?q={self.job}&l={self.location}&start={i}'
+            time.sleep(5)
+            self.driver.save_screenshot("indeed2.png")
+            print(self.url)
             self.save_offers()
             i += 10
-            url = self.driver.current_url.split('start=')[0] + str(i)
-            self.driver.get(url)
             print(len(self.offers))
+            
