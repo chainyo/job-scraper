b878f0ecbe7d4a14d327f574d3957972bc502c75
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-09-22 07:07:33 -0500

    ⚽ update sidebar

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-# Layout
+# Main layout
 st.set_page_config(
     layout="wide",
     page_title='Job Scraper',
     page_icon=":moneybag:")
 st.title("Job Scraper Dashboard")
 
-# Make 2 input boxes, one for the job title and one for the location
-job_title = st.sidebar.text_input("Job Title", "Data Scientist")
-location = st.sidebar.text_input("Location", "New York")
+# Sidebar
+job_title = st.sidebar.text_input("Job Title", "ex: Data Scientist")
+location = st.sidebar.text_input("Location", "ex: Paris")
+
+if st.sidebar.button("Start Scraping"):
+    # Scraping logic here
